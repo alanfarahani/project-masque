6ae8519930e03db6099b8f9a28475f436c8211f8
Aaron Marisi <aaronmarisi@users.noreply.github.com>
2026-02-01 02:14:39 -0500

    base mask and button interactivity

--- Assets/Scripts/UI/UiController.cs
+++ Assets/Scripts/UI/UiController.cs
@@ -7,19 +7,66 @@ public class UiController: MonoBehaviour
 	
 	//game start is really debug, TODO update this
 	public VisualElement mainContainer { get; private set; }
-    
-    public UiController(UIDocument doc)
-	{
-		document = doc;
-	}
 
     public void exposeGameStartUI()
     {
-        mainContainer = document.rootVisualElement.Q("mainContainer");
+        mainContainer = GetComponent<UIDocument>().rootVisualElement.Q("mainContainer");
     }
 
     void Awake()
     {
-        
+        exposeGameStartUI();
+
+        mainContainer.RegisterCallback<ClickEvent>(evt =>
+        {            
+            var target = evt.target as VisualElement;
+            if (target == null)
+            {
+                return;
+            } 
+
+            if (target.ClassListContains("character-button") && !target.ClassListContains("selected") && !target.ClassListContains("completed"))
+            {   
+                ActivateCharacter(target);
+            }
+        });
+
+    }
+
+    public void ActivateCharacter(VisualElement button)
+    {
+        mainContainer.Query(className: "character-button").ForEach(element =>
+        {
+            if (element.name == button.name)
+            {
+                element.AddToClassList("selected");
+                return;        
+            }
+
+            element.RemoveFromClassList("selected");
+        });
+    }
+
+    public void EnableMask(int index)
+    {
+        if (index < 0 || index > 3)
+        {
+            return;
+        }
+
+        mainContainer.Q("mask"+index).RemoveFromClassList("hidden");
+    }
+
+    public void MarkCharacterCompleted(int index)
+    {
+        if (index < 0 || index > 2)
+        {
+            return;
+        }
+
+        var buttonName = "characterButton" + index;
+        mainContainer.Q(buttonName).RemoveFromClassList("selected");
+        mainContainer.Q(buttonName).AddToClassList("completed");        
     }
 }
+
